890b9d627608d6ea4788ed21e6fd9fb4026cbe54
LincolnK <lincoln.kinley@siu.edu>
2017-03-01 14:38:14 -0600

    Update main.c

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 #include "LincolnFunctions.h"
 #include "BreannFunctions.h"
 #include "AndreaFunctions.h"
-#include "SarahFucntions.h"
+#include "SarahFunctions.h"
 
 void main()
 {
@@ -22,52 +22,52 @@ void main()
     switch (input) {
         
       case '1' :
-        printf("Run Function 1\n"); \\Login
+        printf("Run Function 1\n"); //Login
         break;
         
       case '2' :
-        printf("Run Function 2\n"); \\Logout, save, and exit
+        printf("Run Function 2\n"); //Logout, save, and exit
         program = 0;
         break;
         
       case '3' :
-        printf("Run Function 3\n"); \\View Timetable (student)
+        printf("Run Function 3\n"); //View Timetable (student)
         break;
         
       case '4' :
-        printf("Run Function 4\n"); \\View Personal Attendance (student)
+        printf("Run Function 4\n"); //View Personal Attendance (student)
         break;
         
       case '5' :
-        printf("Run Function 5\n"); \\View all attendance (student)
+        printf("Run Function 5\n"); //View all attendance (student)
         break;
         
       case '6' :
-        printf("Run Function 6\n"); \\View Submissions (student)
+        printf("Run Function 6\n"); //View Submissions (student)
         break;
         
       case '7' :
-        printf("Run Function 7\n"); \\View Vacancies (student)
+        printf("Run Function 7\n"); //View Vacancies (student)
         break;
         
       case '8' :
-        printf("Run Function 8\n"); \\Input Student Attendance (Faculty)
+        printf("Run Function 8\n"); //Input Student Attendance (Faculty)
         break;
         
       case '9' :
-        printf("Run Function 9\n"); \\Update Timetable (Admin)
+        printf("Run Function 9\n"); //Update Timetable (Admin)
         break;
         
       case 'A' :
-        printf("Run Function A\n"); \\Post vacancies (Admin)
+        printf("Run Function A\n"); //Post vacancies (Admin)
         break;
         
       case 'B' :
-        printf("Run Function B\n"); \\view Details of students (Faculty)
+        printf("Run Function B\n"); //view Details of students (Faculty)
         break;
         
       case 'C' :
-        printf("Run Function C\n"); \\Edit exam timetable (Faculty)
+        printf("Run Function C\n"); //Edit exam timetable (Faculty)
         break;
         
       default :
