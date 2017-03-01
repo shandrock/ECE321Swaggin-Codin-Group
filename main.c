ecbca151d9dbe7e8266d9d704dfbcf91bd7982de
LincolnK <lincoln.kinley@siu.edu>
2017-03-01 14:07:40 -0600

    Update main.c

--- main.c
+++ main.c
@@ -10,4 +10,6 @@
 void main()
 {
   printf("This is the best program, all others are bad\n");
+  int num = 3;
+  int z = run(num); // This will set z to be num + 5, since the run fuction adds five to whatever number it is passed
 }
